691b8e4bbe9618dddcfb01860ab55d482f7ac08e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2024-05-31 15:05:36 +0100

    WIP : TM field for arbitrary mnp.
    Former-commit-id: 58452c9056ef0dbf2698a9a506452f963b0e2086

--- include/BDSFieldType.hh
+++ include/BDSFieldType.hh
@@ -57,7 +57,8 @@ struct fieldtypes_def
 	     skewmultipoleouteroctupole, skewmultipoleouterdecapole,
 	     multipoleouterdipole3d,
 	     multipoleouterdipolelhc, multipoleouterquadrupolelhc,
-	     multipoleoutersextupolelhc
+	     multipoleoutersextupolelhc,
+             transversemagnetic
   };
 };
 
