e6dc6fa358a47cf1e0d8030bde09727da64a6e2a
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2024-06-03 12:19:09 +0100

    WIP : change gain to phase. Add comparison test
    Former-commit-id: 44373c46162bd9ff75a617a54bd909e174565121

--- include/BDSCavityFieldType.hh
+++ include/BDSCavityFieldType.hh
@@ -31,7 +31,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 
 struct cavityfieldtypes_def
 {
-  enum type {constantinz, pillbox};
+  enum type {constantinz, pillbox, transversemagnetic};
 };
 
 typedef BDSTypeSafeEnum<cavityfieldtypes_def,int> BDSCavityFieldType;
