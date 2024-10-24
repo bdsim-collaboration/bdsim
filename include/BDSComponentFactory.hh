58a73cc24ed06623c7b58a84ac19a1914a924698
marindeniaud <pkap009@live.rhul.ac.uk>
2024-10-24 14:13:31 +0100

    Add a gas jet element. Box of gas inside a beampipe that can be oriented and xy offset
    Former-commit-id: 5f1c79c0ebf2929b25b9e11bdc80a180ee97cb0f

--- include/BDSComponentFactory.hh
+++ include/BDSComponentFactory.hh
@@ -278,6 +278,7 @@ private:
   BDSAcceleratorComponent* CreateRectangularCollimator();
   BDSAcceleratorComponent* CreateBeamMaskCollimator();
   BDSAcceleratorComponent* CreateGasCapillary();
+  BDSAcceleratorComponent* CreateGasJet();
   BDSAcceleratorComponent* CreateTarget();
   BDSAcceleratorComponent* CreateEllipticalCollimator();
   BDSAcceleratorComponent* CreateJawCollimator();
