a88227a139c37bd52279783c4c2d76c35156afc4
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-12-02 21:35:55 +0000

    acceleration : code formatting and tidying

--- src/BDSBeamlineIntegral.cc
+++ src/BDSBeamlineIntegral.cc
@@ -102,6 +102,10 @@ void BDSBeamlineIntegral::Integrate(const GMAD::Element& componentAsDefined)
               dEk = particleCharge * eField * thisComponentArcLength * transitTimeFactor;
               break;
             }
+          case BDSCavityFieldType::axialstandingapprox:
+            {
+              //dEk = particleCharge *;
+            }
           default:
             {break;}
           }
