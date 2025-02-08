d56763e2b859111302897c9b197b1487089464a8
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-02-08 16:15:23 +0000

    CI/CD: fix python unlink

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# explicitly remove all conflicting python3.13 files
+brew unlink python@3.13
+
+#rm /usr/local/bin/idle3
+#rm /usr/local/bin/pydoc3.13
+#rm /usr/local/bin/python3
+#rm /usr/local/bin/python3-config
+#rm /usr/local/bin/python3.13
+#rm /usr/local/bin/python3.13-config
+
 brew update
-brew upgrade --force
+brew upgrade
 
 # install non ROOT deps
 brew install wget clhep bison xerces-c root
 
-# explicitly remove all conflicting python3.13 files
-# rm /usr/local/bin/idle3
-# rm /usr/local/bin/pydoc3.13
-# rm /usr/local/bin/python3
-# rm /usr/local/bin/python3-config
-#rm /usr/local/bin/python3.13
-# rm /usr/local/bin/python3.13-config
+
 
 # install root
 # brew install root
