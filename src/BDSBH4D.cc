878f099bd67d7af53f8638c9b54237788202a736
Laurie Nevay <laurie.nevay@cern.ch>
2025-05-11 22:09:53 +0200

    Fix include that should only be there for geant4 build.

--- src/BDSBH4D.cc
+++ src/BDSBH4D.cc
@@ -17,7 +17,9 @@ You should have received a copy of the GNU General Public License
 along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "BDSBH4D.hh"
+#ifndef __ROOTBUILD__
 #include "BDSHistBinMapper.hh"
+#endif
 
 #include "TFile.h"
 
