4090beeab9bdaae4a41ac80d91cb13c375175b1f
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-06-28 16:08:43 +0100

    rftrack: required track method

---
+++ python/rftrack/bdsim_element.py
@@ -0,0 +1,8 @@
+import RF_Track
+
+class BDSIMElement(RF_Track.Element) :
+    def __init__(self):
+        pass
+
+    def track(self, particle, S, start_step, end_step, number_of_steps, theead):
+        pass
