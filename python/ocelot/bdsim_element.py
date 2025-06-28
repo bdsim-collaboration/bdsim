8fb2e1e2a2ccd12174a802b648b563aa80ccafd2
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-06-28 15:03:11 +0100

    ocelot: add more required methods/members

--- python/ocelot/bdsim_element.py
+++ python/ocelot/bdsim_element.py
@@ -1,4 +1,10 @@
+import ocelot
+class BDSIMElement(ocelot.cpbd.elements.optic_element.Element) :
+    def __init__(self, l, eid=None):
+        self.l = l
+        super().__init__(eid=eid, has_edge=False)
 
-class BDSIMElement() :
-    def __init__(self):
+        self.tms = []
+
+    def set_tm(self, tm_class_type):
         pass
