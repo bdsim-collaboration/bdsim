26867c684388918e889f5228b4ceba6c3336bf8a
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-10-12 10:38:29 +0100

    pybind11: don't upgrade packages

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -6,9 +6,6 @@ brew update
 # --force-bottle python as other method fails with link error
 brew install python@3.13 --force-bottle
 
-# upgrade packages
-brew upgrade
-
 # install BDSIM deps
 brew install wget clhep bison xerces-c root hdf5
 
