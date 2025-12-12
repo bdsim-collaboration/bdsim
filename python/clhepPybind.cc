b28b8576474c59c6083b2578eb9bbab20947650b
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-12-12 22:50:08 +0000

    pybind11: add extra energy units

--- python/clhepPybind.cc
+++ python/clhepPybind.cc
@@ -31,7 +31,11 @@ namespace py = pybind11;
 #include "CLHEP/Units/PhysicalConstants.h"
 
 PYBIND11_MODULE(clhep, m) {
+  m.attr("keV") = py::cast(CLHEP::keV);
+  m.attr("MeV") = py::cast(CLHEP::MeV);
   m.attr("GeV") = py::cast(CLHEP::GeV);
+  m.attr("TeV") = py::cast(CLHEP::TeV);
+  m.attr("PeV") = py::cast(CLHEP::PeV);
 
   m.attr("nanometer") = py::cast(CLHEP::nanometer);
   m.attr("nm") = py::cast(CLHEP::nanometer);
