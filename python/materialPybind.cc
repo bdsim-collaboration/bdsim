55d2c1cdee4f492e66baa1fd253a3ad757f75878
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-06 13:46:57 +0100

    python: remove m shadow for module import

--- python/materialPybind.cc
+++ python/materialPybind.cc
@@ -101,8 +101,8 @@ PYBIND11_MODULE(material, m) {
   .def("__setitem__", [](GMAD::Material &self, const std::string& key, const std::string& value) {self.set_value(key, value, false);})
   .def("__setitem__", [](GMAD::Material &self, const std::string& key, GMAD::Array *value) {self.set_value(key, value, false);})
   .def("__setitem__", [](GMAD::Material &self, const std::string& key, py::list &value) {
-    py::module_ m = py::module_::import("bdsim");
-    py::object cls = m.attr("Array");  // get the class
+    py::module_ mbdsim = py::module_::import("bdsim");
+    py::object cls = mbdsim.attr("Array");  // get the class
     py::object obj = cls(value);       // call constructor
     auto array = obj.cast<GMAD::Array*>();
     self.set_value(key, array, false);
--- python/samplerplacementPybind.cc
+++ python/samplerplacementPybind.cc
@@ -126,8 +126,8 @@ PYBIND11_MODULE(samplerplacement, m) {
     .def("__setitem__", [](GMAD::SamplerPlacement &self, const std::string& key, double value) {self.set_value(key,value, false);})
     .def("__setitem__", [](GMAD::SamplerPlacement &self, const std::string& key, const std::string& value) {self.set_value(key, value, false);})
     .def("__setitem__", [](GMAD::SamplerPlacement &self, const std::string& key, py::list &value) {
-      py::module_ m = py::module_::import("bdsim");
-      py::object cls = m.attr("Array");  // get the class
+      py::module_ mbdsim = py::module_::import("bdsim");
+      py::object cls = mbdsim.attr("Array");  // get the class
       py::object obj = cls(value);       // call constructor
       auto array = obj.cast<GMAD::Array*>();
       self.set_value(key, array, false);
