2450c98923eb0c8157e074d44fe16850d7805adc
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-11 19:52:58 +0100

    options: add flags to remove primary first/last hit and summary from event tree

--- parser/optionsBase.h
+++ parser/optionsBase.h
@@ -351,7 +351,10 @@ namespace GMAD
     bool        storeParticleData;
     bool        storePrimaries;
     bool        storePrimaryHistograms;
-    
+    bool        storePrimaryFirstHit;
+    bool        storePrimaryLastHit;
+    bool        storeSummary;
+
     bool        storeTrajectory;
     // filters
     int         storeTrajectoryDepth;
